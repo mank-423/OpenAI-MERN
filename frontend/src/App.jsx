28e8f99ba834657782d8e227b1aea49a23bd77b3
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-12-11 20:39:00 +0530

    Basic UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:5000/chat', { prompt })
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='w-3/4 mx-auto py-24'>
+      <div className='w-full max-w-md mx-auto'>
+        <form className='text-center' onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <label htmlFor='prompt' className='block text-sm font-medium text-gray-700'>
+              Just say/ask something
+            </label>
+            <textarea
+              id='prompt'
+              name='prompt'
+              rows='4'
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              className='mt-1 p-2 block w-full border rounded-md bg-gray-100 focus:outline-none focus:border-blue-500'
+              placeholder='Type your message...'
+              required
+            ></textarea>
+          </div>
 
-export default App
+          <div className='flex items-center justify-center'>
+            <button
+              type='submit'
+              className='w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700'
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+
+        {response && (
+          <div className='mt-8'>
+            <p className='text-lg font-semibold'>Response from OpenAI:</p>
+            <div className='p-4 border rounded-md bg-gray-100'>{response}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
